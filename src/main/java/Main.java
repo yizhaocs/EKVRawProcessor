d16b9a8adea44bed4c593bbf8e5b9d0154bcbcab
yzhao <yi.zhao@adara.com>
2017-04-26 11:48:34 -0700

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import org.apache.commons.lang3.text.StrTokenizer;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -5,6 +7,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -14,8 +17,12 @@ public class Main {
 
 
     public static void ekvraw(){
+        StrTokenizer st = StrTokenizer.getCSVInstance();
+        st.setDelimiterChar('|');
+
+
         File file = new File("/Users/yzhao/Desktop/table_dump_outout.csv");
-        File fout = new File("/Users/yzhao/Desktop/" + "somehing.csv");
+        File fout = new File("/Users/yzhao/Desktop/" + "result.csv");
         Scanner scanner = null;
         FileOutputStream fos = null;
         BufferedWriter bw = null;
@@ -25,6 +32,28 @@ public class Main {
             fos = new FileOutputStream(fout);
             bw = new BufferedWriter(new OutputStreamWriter(fos));
             while (scanner.hasNextLine()) {
+                String line = scanner.next();
+                String[] segments = null;
+                if(line != null)
+                segments = st.reset(line).getTokenArray();
+                String EVENT_ID = null;
+                String KEY_ID = null;
+                String VALUE = null;
+                String COOKIE_ID = null;
+                String DP_ID = null;
+                String LOCATION_ID = null;
+                String MODIFICATION_TS = null;
+
+                EVENT_ID = segments[0];
+                KEY_ID = segments[1];
+                VALUE = segments[2];
+                COOKIE_ID = segments[3];
+                DP_ID = segments[4];
+                LOCATION_ID = segments[5];
+                MODIFICATION_TS = segments[6];
+
+
+
                 String cookieID = scanner.nextLine().trim();
                 // ckvraw|20|103467387633636100|2044=;12345|103467387633636100|80|80|80
                 bw.write("ckvraw" + "|" + time_stamp + "|" + cookieID + "|" + key + "=" + value + "|" + "null" + "|||");
